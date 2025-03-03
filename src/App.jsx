d69b89af5f29af14a78ba05fc28977ba8170b64c
Nurbekprog <nurprogdata@gmail.com>
2025-03-03 09:02:47 +0500

    bizning jamoa section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Example from './components/ourTeam'
 
 const App = () => {
   return (
     <div className=''>
       <Navbar/>
+      <h2 className='text-center text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl'>Bizning jamoa</h2>
+      <Example/>
     </div>
   )
 }
